50ebf2960e5a2212ee039f2dc8bff57bcd5cbd4a
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-08 14:43:19 -0500

    Create Index

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+	
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
