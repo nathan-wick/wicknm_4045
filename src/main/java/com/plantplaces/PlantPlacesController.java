897e89686914fe2695b63efa6d01631429bac598
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-10 11:00:38 -0500

    Postman

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantPlacesController {
@@ -11,8 +13,13 @@ public class PlantPlacesController {
 		return "start";
 	}
 	
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
 		return "start";
 	}
 	
