ed3b8dcf0813e6a9a632543a9afed6a870b44565
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-10 12:52:21 -0500

    Autowired

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,13 +1,20 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
+
 @Controller
 public class PlantPlacesController {
 
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "start";
@@ -15,6 +22,7 @@ public class PlantPlacesController {
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
 	public String read() {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
 		return "start";
 	}
 	
